5807f81c04bb25130971ca8eee828faca75f3cff
Paul Popus <paul@nouance.io>
2023-11-11 15:25:27 +0000

    Fix code block

--- next/src/components/blocks/Code.tsx
+++ next/src/components/blocks/Code.tsx
@@ -13,9 +13,12 @@ function CodeBlock({ language, code }: CodeBlockProps) {
       <div className={''}>
         <Highlight theme={themes.dracula} code={code} language={languageToUse}>
           {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={`${className} w-full max-w-[100%] overflow-scroll pt-7`} style={style}>
+            <pre
+              className={`${className} p-4  w-full max-w-[100%] overflow-auto pt-7`}
+              style={style}
+            >
               {tokens.map((line, i) => (
-                <div {...getLineProps({ line, key: i })} key={i} className={'table-cell p-4'}>
+                <div {...getLineProps({ line, key: i })} key={i} className={'table-row p-4'}>
                   <span className={'text-right table-cell select-none'}>{i + 1}</span>
                   <span className={'pl-4 table-cell'}>
                     {line.map((token, key) => (
